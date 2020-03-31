07957657cae3fc89cac9106c9ec31bd043c72661
Kimberly W <kimberlywiebeling@gmail.com>
2020-03-31 19:13:56 -0300

    Aula 05 Finalizada

--- public/scripts.js
+++ public/scripts.js
@@ -14,3 +14,26 @@ function onOff() {
         .classList
         .toggle("addScroll")
 }
+
+//Para não deixar salvar sem ter todos os dados preenchidos no formulário
+function checkFields(event){
+    const valuesToCheck =[
+        "title",
+        "category",
+        "image",
+        "description",
+        "link"
+    ]
+    const isEmpty = valuesToCheck.find(function(value){
+        const checkIfIsString = typeof event.target[value].value === "string"
+        const checkIfIsEmpty = !event.target[value].value.trim()
+        if(checkIfIsString && checkIfIsEmpty){
+            return true
+        }
+    })
+
+    if (isEmpty){
+        event.preventDefault()
+        alert("Por favor preeencha todos os campos")
+    }
+}
